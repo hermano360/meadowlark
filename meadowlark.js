013db1eabfb88fa29c8f3ecc2e346840ec2cdc8f
Herminio Garcia <hermano360@gmail.com>
2017-03-16 20:16:10 -0700

    added newsletter with form handling about

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
-
-
+var formidable = require('formidable');
+var jpupload = require('jquery-file-upload-middleware');
 var app = express();
 
 //set up handlebars view engine
@@ -25,6 +25,8 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(require('body-parser').urlencoded({extended:true}));
+
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
 	res.locals.showTests = app.get('env') !== 'production' && 
@@ -42,21 +44,21 @@ function getWeatherData(){
                 forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
                 iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
                 weather: 'Overcast',
-                temp: '54.1 F (12.3 C)',
+                temp: ' 54.1 F (12.3 C)',
             },
             {
-                name: 'Bend',
-                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                name: 'Duarte',
+                forecastUrl: 'http://www.wunderground.com/US/CA/Duarte.html',
                 iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
                 weather: 'Partly Cloudy',
-                temp: '55.0 F (12.8 C)',
+                temp: ' 55.0 F (12.8 C)',
             },
             {
                 name: 'Manzanita',
                 forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
                 iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
                 weather: 'Light Rain',
-                temp: '55.0 F (12.8 C)',
+                temp: ' 55.0 F (12.8 C)',
             },
         ],
     };
@@ -100,6 +102,64 @@ app.get('/data/nursery-rhyme', function(req,res){
 		noun: 'heck'
 	});
 });
+
+app.get('/newsletter', function(req,res){
+	//we will learn about CSRF later..for now we will just provide dummy value
+	res.render('newsletter', { csrf: 'Test Test the real value goes here later' });
+});
+
+app.get('/thank-you', function(req,res){
+	res.render('thank-you');
+});
+
+app.get('/contest/vacation-photo', function(req,res){
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(),month: now.getMonth()
+	});
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req,res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err,fields,files){
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields: ');
+		console.log(fields);
+		console.log('received files: ');
+		console.log(files);
+		res.redirect(303,'/thank-you');
+	});
+});
+
+app.post('/process',function(req,res){
+
+	if(req.xhr || req.accepts('json,html')==='json'){
+		//if there was an error, we should send { error: 'error description'}
+		res.send({success:true});
+	} else {
+		//if there was an eror, we would redirect to an error page
+		res.redirect(303,'/thank-you');
+	}
+	console.log('Form (from querystring): ' + req.query.form);
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Name (from visible form field): ' + req.body.name);
+	console.log('Email (from visible form field): ' + req.body.email);
+});
+
+app.use('/upload', function(req,res,next){
+	var now = Date.now();
+	jpupload.filehHandler({
+		uploadDir: function(){
+			return __dirname + '/public/uploads/' + now;
+		},
+		uploadUrl: function() {
+			return '/uploads/'+now;
+		}
+	})(req,res,next);
+});
+
+
+
 
 
 // custom 404 page
