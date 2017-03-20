b37de641b89d900204ab9a70c282f45ac9549c97
Herminio Garcia <hermano360@gmail.com>
2017-03-19 20:52:52 -0700

    still working

---
+++ models/vacationInSeasonListener.js
@@ -0,0 +1,11 @@
+var mongoose - require('mongoose');
+
+var vacationInSeasonListenerSchema = mongoose.Schema({
+	email: String,
+	skus: [String]
+});
+
+var vacationInSeasonListener = mongoose.model('vacationInSeasonListener',
+	vacationInSeasonListenerSchema);
+
+module.exports = vacationInSeasonListener;
